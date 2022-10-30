2cbad5a2831ffc520d99b80fb35bd1329c2f6b5b
Tom Spencer <thomas.spencer@codurance.com>
2022-10-30 15:53:36 +0000

    Add configuration for swagger

--- src/main/java/com/tomspencerlondon/ParkingLotApplication.java
+++ src/main/java/com/tomspencerlondon/ParkingLotApplication.java
@@ -1,19 +1,31 @@
 package com.tomspencerlondon;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class ParkingLotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingLotApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("EMS API").version(appVersion)
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
